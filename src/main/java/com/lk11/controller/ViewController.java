066204f26f3bfb795daa391d6dd7a079c8465c24
yjlee <dldudwo87@gmail.com>
2020-07-05 09:28:58 +0900

    Add ViewController

--- src/main/java/com/lk11/controller/ViewController.java
+++ src/main/java/com/lk11/controller/ViewController.java
@@ -2,8 +2,11 @@ package com.lk11.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ViewController {
@@ -11,8 +14,29 @@ public class ViewController {
 	private static final Logger log = LoggerFactory.getLogger(ViewController.class);
 	
 	@RequestMapping
-	public String showMainView() {
-		log.info(">> showMainView");
-		return "main";
+	public String showView(Authentication authentication) {
+		log.info(">> showView");
+		if(!isLogin(authentication)) {
+			return "redirect:/login";
+		}
+		return "redirect:/index";
+	}
+	
+	private boolean isLogin(Authentication authentication) {
+		if(null == authentication)
+			return false;
+		return true;
+	}
+
+	@RequestMapping(path = "/login")
+	public String showLoginView(Authentication authentication) {
+		log.info(">> showLoginView");
+		return "login";
+	}
+	
+	@RequestMapping(path = "/index")
+	public String showIndexView() {
+		log.info(">> showIndexView");
+		return "index";
 	}
 }
