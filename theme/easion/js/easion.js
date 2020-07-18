f1e42288e5a9af02f63a74171acc047154808ee5
yjlee <dldudwo87@gmail.com>
2020-07-18 22:52:00 +0900

    ---- Base -----  CSS 반응형 삭제 (임시버전)

--- theme/easion/js/easion.js
+++ theme/easion/js/easion.js
@@ -5,8 +5,6 @@
  * https://mudimedia.com
  */
 
-const mobileBreakpoint = window.matchMedia("(max-width: 991px )");
-
 $(document).ready(function(){
     $(".dash-nav-dropdown-toggle").click(function(){
         $(this).closest(".dash-nav-dropdown")
@@ -18,20 +16,4 @@ $(document).ready(function(){
             .siblings()
             .removeClass("show");
     });
-
-    $(".menu-toggle").click(function(){
-        if (mobileBreakpoint.matches) {
-            $(".dash-nav").toggleClass("mobile-show");
-        } else {
-            $(".dash").toggleClass("dash-compact");
-        }
-    });
-
-    $(".searchbox-toggle").click(function(){
-        $(".searchbox").toggleClass("show");
-    });
-
-    // Dev utilities
-    // $("header.dash-toolbar .menu-toggle").click();
-    // $(".searchbox-toggle").click();
 });
